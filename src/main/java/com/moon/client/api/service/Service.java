33d37d5808113728782179e0961d7e5469c7f8f1
lennoxlotl <admin@lennoxlotl.dev>
2024-11-23 14:23:48 +0100

    feat: service registry

---
+++ src/main/java/com/moon/client/api/service/Service.java
@@ -0,0 +1,14 @@
+package com.moon.client.api.service;
+
+/**
+ * Services expose publicly accessible APIs used to execute internal functionality.
+ * <p>
+ * Every service should have a base class that can be implemented to define the required functionality.
+ * It is not recommended to directly implement the Service class into the implementation of a service.
+ * There should always be a high-level api interface.
+ *
+ * @author lennoxlotl
+ * @since 1.0.0
+ */
+public interface Service {
+}
