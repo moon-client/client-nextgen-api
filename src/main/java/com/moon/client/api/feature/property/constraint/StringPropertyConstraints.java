7010150311120105e67ae294abbcc052c6ae3a7a
lennoxlotl <admin@lennoxlotl.dev>
2025-03-10 14:36:34 +0100

    chore: static method for creation

--- src/main/java/com/moon/client/api/feature/property/constraint/StringPropertyConstraints.java
+++ src/main/java/com/moon/client/api/feature/property/constraint/StringPropertyConstraints.java
@@ -1,3 +1,20 @@
+/*
+ * client-nextgen-api
+ * Copyright (C) 2024-2025 Moon Client
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.moon.client.api.feature.property.constraint;
 
 import com.moon.client.api.feature.property.PropertyConstraints;
@@ -10,4 +27,7 @@ import com.moon.client.api.feature.property.PropertyConstraints;
  * @since 1.0.0
  */
 public record StringPropertyConstraints(boolean word) implements PropertyConstraints {
+    public static StringPropertyConstraints of(boolean word) {
+        return new StringPropertyConstraints(word);
+    }
 }
